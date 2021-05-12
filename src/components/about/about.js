bcb9de7e0fbe37a210fea881256fc2ec2e377c96
Fatih <fatihce57@gmail.com>
2021-05-12 16:55:42 +0200

    about image  is placed

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,13 +1,22 @@
-import './about.css';
+import "./about.css";
+// import React from 'react';
 
 function About() {
   return (
     <div className="about">
-     <h2 id="abouthead">About</h2>
-
-
-
-
+      <h2 id="abouthead">About</h2>
+<div className="container-about">
+      <img className="about-img" src="./images/fatihphoto.jpg" alt="" />
+      <p className="about-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi incidunt,
+        consectetur inventore at, quam illo sint facilis praesentium nam dicta,
+        consequatur veritatis asperiores temporibus soluta sapiente rerum
+        necessitatibus aspernatur in. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Animi incidunt, consectetur inventore at, quam illo
+        sint facilis praesentium nam dicta, consequatur veritatis asperiores
+        temporibus soluta sapiente rerum necessitatibus aspernatur in.
+      </p>
+    </div>
     </div>
   );
 }
