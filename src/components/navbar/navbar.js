76f912c8e9f17ef00b6535bb55a495376839f4a1
Fatih <fatihce57@gmail.com>
2021-05-21 02:16:33 +0200

    navbar with react bootstrap responsive done

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,17 +1,18 @@
 import "./navbar.css";
+import { Nav, Navbar, Row, Col } from 'react-bootstrap';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
-
-function Navbar() {
+function Navigation() {
   return (
    
      
-        <nav className="container-navbar">
+        <nav id="top" className="container-navbar">
  
         {/* <div>
         <img className="logo" src="./images/logo.png" alt="" />
         </div> */}
 
-         <div class="collapse" id="navbarToggleExternalContent">
+         {/* <div class="collapse" id="navbarToggleExternalContent">
   <div class="p-4">
     
     <nav class="text-white h4">
@@ -41,9 +42,36 @@ function Navbar() {
       <span class="navbar-toggler-icon"></span>
     </button>
   </div>
-</nav> 
+</nav>  */}
 
 
+<Row>
+  
+<Navbar className="fixed-top fw-bold fs-4" collapseOnSelect expand="lg" variant="dark">
+  <Navbar.Brand className="fw-bold fs-4 text-danger" href="#top">Home</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+  
+  <Col>
+    <Nav className="mr-auto">
+      <Nav.Link className="text-danger" href="#abouthead">About</Nav.Link>
+      <Nav.Link className="text-danger" href="#projecthead">Project</Nav.Link>
+      <Nav.Link className="text-danger" href="#skillshead">Skills</Nav.Link>
+    </Nav>
+    </Col>
+    <Col >
+    <Nav className="sos-med">
+    <Nav.Link className="text-warning" href="#contacthead">Contact</Nav.Link>
+    <Nav.Link href="#/"><h3 className="sos_med"><FaFacebookF /></h3></Nav.Link>
+    <Nav.Link href="#/"><h3 className="sos_med"><FaTwitter /></h3></Nav.Link>
+    <Nav.Link href="https://www.linkedin.com/feed/" target="_blank"><h3 className="sos_med"><FaLinkedinIn /></h3></Nav.Link>
+    <Nav.Link href="https://github.com/Fatihce57" target="_blank"><h3 className="sos_med"><FaGithub /></h3></Nav.Link>
+    </Nav>
+    </Col>
+  </Navbar.Collapse>
+</Navbar>
+
+</Row>
 
 
 
@@ -55,4 +83,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default Navigation;
