ba8f73ad59b0dbc6a699f358a6b33df0b621d319
Fatih <fatihce57@gmail.com>
2021-05-23 14:15:14 +0200

    header media done

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,13 +11,13 @@ function Header() {
 
 <Container className="text">
   <Row>
-    <Col xs={10} md={11} lg={12}><div class="speed" id="linear"><h1 id="hello">Hello <span className="exclamation">!</span></h1></div></Col>
+    <Col className="head-res" xs={10} sm={10} md={11} lg={12}><div class="speed" id="linear"><h1 id="hello">Hello <span className="exclamation">!</span></h1></div></Col>
    </Row>
   <Row>
-    <Col xs={10} md={11} lg={12}><div class="speed" id="ease"><h2 className="fatihce">I'm <span className="header_fatih">Fatih</span> <span className="header_punkt"> <IoChatbubbleEllipses /></span></h2></div></Col>
+    <Col className="head-res" xs={10} sm={10} md={11} lg={12}><div class="speed" id="ease"><h2 className="fatihce">I'm <span className="header_fatih">Fatih</span> <span className="header_punkt"> <IoChatbubbleEllipses /></span></h2></div></Col>
    </Row>
    <Row>
-    <Col xs={10} md={11} lg={12}><div class="speed" id="easeIn"> <h2 className="stack">I'm a full-stack web developer <br /> candidate  <span className="smileicon"><BiWinkSmile /></span></h2></div></Col>
+    <Col className="head-res" xs={10} sm={10} md={11} lg={12}><div class="speed" id="easeIn"> <h2 className="stack">I'm a full-stack web developer <br /> candidate  <span className="smileicon"><BiWinkSmile /></span></h2></div></Col>
    </Row>
 </Container>
 
