f3ac1357f1338b3b81f080056a939bef226b83f5
Fatih <fatihce57@gmail.com>
2021-11-22 00:47:44 +0100

    contact scss done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import './Contact.scss'
 import { Container, Row, Col } from 'react-bootstrap'
-import { FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa'
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPaperPlane,
+  FaEnvelopeOpenText,
+} from 'react-icons/fa'
 import emailjs from 'emailjs-com'
 import { init } from 'emailjs-com'
 init('user_zyFQ1I52eq7fo5ZtPBvL1')
@@ -48,7 +53,8 @@ function Contact() {
   }
 
   const isValidEmail = (email) => {
-    const regex =/^(([^<>()/[\]\\.,;:\s@"]+(\.[^<>()/[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const regex =
+      /^(([^<>()/[\]\\.,;:\s@"]+(\.[^<>()/[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return regex.test(String(email).toLowerCase())
   }
 
@@ -61,80 +67,98 @@ function Contact() {
         <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
     */}
 
-   
+      <Row class="contact-wrapper">
+        <Col id="contact-form" class="form-horizontal" role="form">
+          <div class="form-group">
+            <div class="col-sm-12">
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                placeholder="Name"
+                name="name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
 
+          <div class="form-group">
+            <div class="col-sm-12">
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="E-Mail"
+                name="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
 
+          <div class="form-group">
+            <div class="col-sm-12">
+              <input
+                type="text"
+                class="form-control"
+                id="number"
+                placeholder="Tel Number"
+                name="email"
+                required
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+              />
+            </div>
+          </div>
 
+          <textarea
+            class="form-control"
+            rows="10"
+            placeholder="Your Message"
+            name="message"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
 
-  
-  <div class="contact-wrapper">
-  
+          <button
+            class="btn btn-warning send-button"
+            id="submit"
+            type="submit"
+            value="SEND"
+            onClick={submit}
+          >
+            <div class="alt-send-button">
+              <FaEnvelopeOpenText />
+              <span class="send-text">
+                <FaPaperPlane />
+              </span>
+            </div>
+          </button>
+        </Col>
 
-    
-    <form id="contact-form" class="form-horizontal" role="form">
-       
-      <div class="form-group">
-        <div class="col-sm-12">
-          <input type="text" class="form-control" id="name" placeholder="NAME" name="name" required value={name} onChange={e => setName(e.target.value)}/>
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="col-sm-12">
-          <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" required value={email} onChange={e => setEmail(e.target.value)}/>
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="col-sm-12">
-          <input type="number" class="form-control" id="number" placeholder="NUMBER" name="email" required value={number} onChange={e => setNumber(e.target.value)}/>
-        </div>
-      </div>
-
-      <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message" required value={message} onChange={e => setMessage(e.target.value)}></textarea>
-      
-      <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND" onClick={submit}>
-        <div class="alt-send-button">
-          <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
-        </div>
-      
-      </button>
-      
-    </form>
-    
-
-    
-      <div class="direct-contact-container">
-
-        <ul class="contact-list">
-        <li className="address">
+        <Col class="direct-contact-container">
+          <ul class="contact-list">
+            <li className="address">
               <FaMapMarkerAlt /> Berlin, Germany
-            </li>     
-         <li className="email">
+            </li>
+            <li className="email">
               <FaEnvelope /> fatihce57@gmail.com
             </li>
-
+            <hr />
             <li>
-            <h3 className="aboutme">Informations </h3>
-            <p>
-              You can always contact me via my social media accounts or mail. I
-              will give you feedback within 1 day.
-            </p>
+              <h3 className="aboutme">Informations </h3>
+              <p>
+                You can always contact me via my social media accounts or mail.
+                I will give you feedback within 1 day.
+              </p>
             </li>
-          
-        </ul>
-
-
-
-      </div>
-    
-  </div>
-  
-
-
-
-
-
+          </ul>
+        </Col>
+      </Row>
     </div>
   )
 }
